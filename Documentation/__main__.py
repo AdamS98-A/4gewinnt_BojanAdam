893e587f37ad2e9baf3151f9f15b48d1f472654f
AdamS98-A <s55505@edu.campus02.at>
2025-02-07 15:58:19 +0100

    Created __main__.py
    created __main__.py, the main file that will be run/executed when starting the game

---
+++ Documentation/__main__.py
@@ -0,0 +1,8 @@
+# Main-File of our Game 4Gewinnt
+# by Bojan & Adam
+# this file should be executed when you want to play our game
+# you can run this file or the project directory
+
+
+if __name__ == '__main__':
+    pass
